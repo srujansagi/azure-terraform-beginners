8216ce29c92515fadd37cb370edf6e1cccf19013
srujansagi <srujan.sagi@aa.com>
2018-09-10 20:30:03 +0000

    Updated my name in setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -9,8 +9,11 @@ cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
   <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
-  <center><img src="http://placekitten.com/800/600"></img></center>
-  <marquee><h1>Meow World</h1></marquee>
+  <center><img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiQ65OolrHdAhXF0FMKHYIbCtQQjRx6BAgBEAU&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.aa.android%26hl%3Den_US&psig=AOvVaw10sh-J9lIwYr0pBiDZkB-R&ust=1536694241712052"></img></center>
+  
+  Hello - Good evening
+  
+  <marquee><h1>Copyrights!! Sagi-Srujan</h1></marquee>
   </body>
 </html>
 EOM
